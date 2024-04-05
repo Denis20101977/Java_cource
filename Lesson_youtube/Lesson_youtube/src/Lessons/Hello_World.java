31b512e58f4bf1e6733c9d6131bb3fd8a8519fba
Denis20101977 <944664@mail.ru>
2024-04-05 21:39:35 +1000

    First commit

---
+++ Lesson_youtube/Lesson_youtube/src/Lessons/Hello_World.java
@@ -0,0 +1,7 @@
+package Lessons;
+
+public class Hello_World {
+    public static void main(String [] args) {
+        System.out.printf("Hello World!");
+    }
+}
---
+++ Lesson_youtube/Lesson_youtube/src/Lessons/Virables.java
@@ -0,0 +1,12 @@
+package Lessons;
+
+public class Virables {
+    public static void main(String[] args) {
+        int myInt = 558;
+        long myLong = 5555;
+
+    // Выводим в отдельные строки - разобараться почеум не работает в одну строку?
+        System.out.println("Значение переменной myInt: " + myInt);
+        System.out.println("Значение переменной myLong: " + myLong);
+    }
+}

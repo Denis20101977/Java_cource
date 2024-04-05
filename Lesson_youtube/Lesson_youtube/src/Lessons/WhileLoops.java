5a87748eea2b3047edcd24fafe09887e56526f16
Denis20101977 <944664@mail.ru>
2024-04-05 22:27:33 +1000

    WhileLoops commit

---
+++ Lesson_youtube/Lesson_youtube/src/Lessons/WhileLoops.java
@@ -0,0 +1,12 @@
+package Lessons;
+
+public class WhileLoops {
+    public static void main(String[] args) {
+        int value = 0;
+        while (value<5) {
+            System.out.println("Hello " + value);
+            value = value +1;
+        }
+// Цикл выполянеется до тек пол пока условие истино.
+    }
+}

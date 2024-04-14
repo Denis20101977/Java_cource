209ccbab005d0bc91b7b3d4329efae65ab3762c7
Denis20101977 <944664@mail.ru>
2024-04-14 14:11:18 +1000

    Commit 14/04/2024_2

---
+++ Lesson_youtube/Lesson_youtube/src/Stepik_lessons/floatDouble.java
@@ -0,0 +1,15 @@
+package Stepik_lessons;
+
+public class floatDouble {
+    public static void main(String[] args) {
+//        float radius = 10.8f;
+//        float pi = 3.14f;
+//        float ares = pi * radius * radius;
+        int mother = 55;
+        int father = 46;
+        int summOld = mother + father;
+        double average = (double) summOld/2.0;
+
+        System.out.println(average);
+    }
+}

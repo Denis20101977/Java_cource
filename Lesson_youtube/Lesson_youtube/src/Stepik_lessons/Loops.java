9bdc8bf5e94a77cb5f7a99e672996dcd7b4eef32
Denis20101977 <944664@mail.ru>
2024-04-15 22:46:42 +1000

    Сommit 15.04.2024

---
+++ Lesson_youtube/Lesson_youtube/src/Stepik_lessons/Loops.java
@@ -0,0 +1,39 @@
+package Stepik_lessons;
+
+public class Loops {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 1000) {
+            System.out.println(i);
+            i++;
+
+
+//        int i = 0;
+//        while (i <= 1000) {
+//            System.out.println(i);
+//            i += 2;
+//        int a = 0;
+//        do {
+//            System.out.println("Hello");
+//        }while (a > 0);
+
+//        for (int i = 1; i <= 1000; i++) {
+//            System.out.println(i);
+
+//        for (int i = 1000; i >= 0; i--) {
+//            if (i% 3 == 0)
+//                System.out.println(i);
+
+
+        }
+
+        }
+
+                   }
+
+
+
+
+
+
+
---
+++ Lesson_youtube/Lesson_youtube/src/Stepik_lessons/Strings.java
@@ -0,0 +1,15 @@
+package Stepik_lessons;
+
+public class Strings {
+    public static void main(String[] args) {
+//        String hello = "Hello ";
+//        String world = "World";
+//        String result = hello + world + "!";
+//        System.out.println(result);
+
+        String name = "John";
+        int years = 37;
+        System.out.println("Hello " + name + " \n тебе " + years + " лет");
+
+    }
+}
